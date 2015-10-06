5cf6b732d1096bdceacd893841418f00a768d484
Ivan-Louis <ivan.louis@gmail.com>
2015-10-06 14:22:22 +0200

    no message

--- code/task.py
+++ code/task.py
@@ -8,10 +8,21 @@ from sklearn.cluster import AgglomerativeClustering
 
 input_dir = "../training"
 
+
 #print("Line")
 for name in os.listdir(input_dir):
     #print(name)
     streng = input_dir + "/" + name + "/docs"
     #print(streng)
+    #name = []
+    j = -1
     for i in os.listdir(streng):
         print(name + ": " + i)
+        j += 1
+        #name.append(i)
+        #print(name[j])
+
+    #print(j)
+    #print(name[40])
+
+#print(Abby_Watkins)
