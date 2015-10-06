bfae239542f45e166be129c82e2fc6c4e2a116e9
Gustav Nødland <gustav.nodland@gmail.com>
2015-10-06 12:09:37 +0200

    Totally working parser !!!!!

--- code/html_handle.py
+++ code/html_handle.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from html.parser import HTMLParser
-import csv
+import lxml
+from lxml.html.clean import Cleaner
 
 
 class MyHTMLParser(HTMLParser):
@@ -8,25 +9,29 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.vocabulary = []
+        self.current_tag = 0
 
     def get_vocabulary(self):
         return self.vocabulary
 
+    def handle_starttag(self, tag, attrs):
+        self.current_tag = tag
+
     def handle_data(self, data):
-        data_words = data.split()
-        if len(data_words) >= 10:
-            for i in range(len(data_words)):
-                self.vocabulary.append(data_words[i])
-
-
+        if self.current_tag != "script" and self.current_tag != "style":
+            data_words = data.split()
+            if len(data_words) >= 10:
+                for i in range(len(data_words)):
+                    self.vocabulary.append(data_words[i])
 
 
 def handle_html(path):
     file = open(path)
     html = file.read()
+    print(html)
     parser = MyHTMLParser()
     parser.feed(html)
     return parser.get_vocabulary()
 
-print (handle_html("001.html"))
+print(handle_html("001.html"))
 
