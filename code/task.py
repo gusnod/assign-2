d4c037256edbab5f0a5dfb9d349552610a0ab4ef
Ivan-Louis <ivan.louis@gmail.com>
2015-10-06 13:08:15 +0200

    Skrevet ned gjennomgang av alle filer

--- code/task.py
+++ code/task.py
@@ -1,31 +1,17 @@
 from __future__ import division
 __author__ = 'Louis'
 
+import os
 import numpy as np
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import AgglomerativeClustering
 
+input_dir = "../training"
 
-class MyHTMLParser(HTMLParser):
-
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.vocabulary = []
-
-    def get_vocabulary(self):
-        return self.vocabulary
-
-    def handle_data(self, data):
-        data_words = data.split()
-        if len(data_words) >= 10:
-            print(data_words)
-            for i in range(len(data_words)):
-                self.vocabulary.append(data_words[i])
-
-parser = MyHTMLParser()
-html = ('<html><head><title>Test</title></head>'
-        '<body><h1>Parse me because i am looong enough to be parsed, yaay!</h1><p>do not whohoho sdasdfg</p><p>And'
-        'I will be longer than 10 words because we need to test that the vocabulary actually works</p></body></html>')
-parser.feed(html)
-print("Vocabulary")
-print(parser.get_vocabulary())
+#print("Line")
+for name in os.listdir(input_dir):
+    #print(name)
+    streng = input_dir + "/" + name + "/docs"
+    #print(streng)
+    for i in os.listdir(streng):
+        print(name + ": " + i)
