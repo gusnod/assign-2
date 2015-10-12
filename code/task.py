f6fef3ca042da37d434cf3bd02e9af07e0d8917c
Ivan-Louis <ivan.louis@gmail.com>
2015-10-12 11:27:33 +0200

    YAAAY

--- code/task.py
+++ code/task.py
@@ -5,24 +5,29 @@ import os
 import numpy as np
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import AgglomerativeClustering
+from collections import defaultdict
 
 input_dir = "../training"
 
 
+
+d = defaultdict(list)
+i = 0
 #print("Line")
 for name in os.listdir(input_dir):
     #print(name)
     streng = input_dir + "/" + name + "/docs"
     #print(streng)
-    #name = []
-    j = -1
-    for i in os.listdir(streng):
-        print(name + ": " + i)
-        j += 1
+
+    for site in os.listdir(streng):
+        #print(name + ": " + i)
+        #synopsis[].append(site)
+        d[name].append(site)
         #name.append(i)
         #print(name[j])
-
+    i += 1
     #print(j)
     #print(name[40])
 
+print(d("Abby_Watkins"))
 #print(Abby_Watkins)
